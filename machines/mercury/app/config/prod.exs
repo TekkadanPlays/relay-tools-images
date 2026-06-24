cd7a1b1e27030ec287063e74f7e8767ae5abd5ad
Tekkadan <6tekkadan9@gmail.com>
2026-06-24 01:07:35 -0500

    fix: disable force_ssl in production since HAProxy terminates SSL

--- machines/mercury/app/config/prod.exs
+++ machines/mercury/app/config/prod.exs
@@ -9,7 +9,7 @@ config :gc_index_relay, GcIndexRelay.Repo,
 # known as HSTS. If you have a health check endpoint, you may want to exclude it below.
 # Note `:force_ssl` is required to be set at compile-time.
 config :gc_index_relay, GcIndexRelayWeb.Endpoint,
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  # force_ssl: [rewrite_on: [:x_forwarded_proto]],
   exclude: [
     paths: ["/health"],
     hosts: ["localhost", "127.0.0.1"]
